234f36ad2e1f13d3a9bb90009a63d0a48a378fdd
mbogo martin <78340780+martinmbogo-254@users.noreply.github.com>
2022-03-09 13:19:34 +0300

    ...

--- pod/settings.py
+++ pod/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l&_4)s$6!1*bob)wtg_h1b$w8!qm+va+ra18ilwf)zc1kkfok%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
